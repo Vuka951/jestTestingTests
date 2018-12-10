2e844f58b99d229b2fe98939bfc92fe5f43ebb2c
Vukasin Stojanovic <vultashin76@gmail.com>
2018-12-10 12:24:06 +0100

    Added the first test and full setup for app start and jest

---
+++ index.js
@@ -0,0 +1,16 @@
+const  vugleDatabase = [
+    'something.com',
+    'windows.com',
+    'animals.com',
+    'mrrr.com',
+    'catpics.com',
+    'cats.com'
+];
+
+const vugleSearch= (searchInput) => {
+    const matches = vugleDatabase.filter(site => {
+        return site.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+console.log(vugleSearch('mrr'));
