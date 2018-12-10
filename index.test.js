ca7954a73ce808c0a798a12a7752aa5ff5beaa0a
Vukasin Stojanovic <vultashin76@gmail.com>
2018-12-10 13:37:36 +0100

    Test grouping and more tests

--- index.test.js
+++ index.test.js
@@ -7,12 +7,22 @@ dbMock = [
     'doggypics.com'
 ]
 
-it('test if hello is hello', () => {
-    expect('hello').toBe('hello');
-    
+describe('vugleSearch', () => {
+    it('test if hello is hello', () => {
+        expect('hello').toBe('hello');
+    })
 })
 
 it('is searching properly', () => {
     expect(vugleSearch('testtest', dbMock)).toEqual([]);
     expect(vugleSearch('dog', dbMock)).toEqual(['dog.com', 'doggy.com', 'rawdoggy.com',]);
+})
+
+it('work with undefined/null inputs', () => {
+    expect(vugleSearch(undefined, dbMock)).toEqual([]);
+    expect(vugleSearch(null, dbMock)).toEqual([]);
+})
+
+it('does not return more then 3 matches', () => {
+    expect(vugleSearch('.com', dbMock).length).toEqual(3);
 })
